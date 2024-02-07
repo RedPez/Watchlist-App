e19198e9ec0bd764cee4db989bab1d40960b9b65
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2024-02-07 21:12:09 +0000

    first commit component set up

---
+++ Watchlist-Project/src/components/DisplayCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayCard = () => {
+  return (
+    <div>DisplayCard</div>
+  )
+}
+
+export default DisplayCard
---
+++ Watchlist-Project/src/components/OpenButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OpenButton = () => {
+  return (
+    <div>OpenButton</div>
+  )
+}
+
+export default OpenButton
