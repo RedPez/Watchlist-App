7a3e68a50625bef9bbff9f3f1a1029383f17ab25
Nikola84ca <nicolateotorriatte@outlook.com>
2024-02-08 19:37:42 +0000

    started implementing the API functionality in the Searchbar.jsx file and connected it to the Homepage and App

--- Watchlist-Project/src/pages/Homepage.jsx
+++ Watchlist-Project/src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Searchbar from '../components/Searchbar'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>Homepage
+      <Searchbar/>
+    </div>
   )
 }
 
