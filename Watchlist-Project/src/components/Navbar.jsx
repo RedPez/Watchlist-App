b24e60852d40fa0ca01c428799d410a69e54046f
Whit Will <147825238+Whit-Williams@users.noreply.github.com>
2024-02-10 18:31:47 +0000

    added css styles to navbar

--- Watchlist-Project/src/components/Navbar.jsx
+++ Watchlist-Project/src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import './Navbar.css'
 
 function MyNavbar() {
   return (
     <>
       <Navbar>
-        <Container className="d-flex justify-content-between">
+        <Container>
           <Navbar.Brand href="#Home">
             <img
               alt=""
@@ -17,10 +18,10 @@ function MyNavbar() {
             />{" "}
           </Navbar.Brand>
           
-          <Nav>
-            <Nav.Link href="#Home">Home</Nav.Link>
-            <Nav.Link href="#MyReviews">My Reviews</Nav.Link>
-            <Nav.Link href="#Watchlist">Watchlist</Nav.Link>
+          <Nav className="nav-links-group">
+            <Nav.Link className="home-link" href="../src/pages/Homepage.jsx">Home</Nav.Link>
+            <Nav.Link className="reviews-link" href="../src/pages/MyReviews.jsx">My Reviews</Nav.Link>
+            <Nav.Link className="watchlist-link" href="../src/pages/WatchList.jsx">Watchlist</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
