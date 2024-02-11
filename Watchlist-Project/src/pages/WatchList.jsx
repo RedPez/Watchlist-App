49e2e1a4b9fa7c8007baf1a2e362ed2bddff7054
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2024-02-11 13:27:28 +0000

    restructure code to enable search bar to show above hero section and for watchlist components to be consistent with rest of the other components

--- Watchlist-Project/src/pages/WatchList.jsx
+++ Watchlist-Project/src/pages/WatchList.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import WatchlistComponent from "../components/WatchlistComponent"
 import Footer from '../components/Footer'
 
 
 const WatchList = () => {
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+    const storedWatchlist = JSON.parse(localStorage.getItem('watchlist'));
+    if (storedWatchlist) {
+      setWatchlist(storedWatchlist);
+    }
+  }, []);
+
+  const removeFromWatchlist = (id) => {
+    const updatedWatchlist = watchlist.filter(item => item.id !== id);
+    setWatchlist(updatedWatchlist);
+    localStorage.setItem('watchlist', JSON.stringify(updatedWatchlist));
+  };
   return (
     <>
     <Navbar/> 
-    <WatchlistComponent />
+    <WatchlistComponent 
+    watchlist={watchlist}
+    removeFromWatchlist={removeFromWatchlist}
+    />
     <Footer />
     </> 
   )
