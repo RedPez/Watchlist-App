8c633a9af236f019ecfe45e09f7a048770bf307d
Whit Will <147825238+Whit-Williams@users.noreply.github.com>
2024-02-11 18:44:06 +0000

    Adding formatting to modal

--- Watchlist-Project/src/components/ReviewModalWithButton.jsx
+++ Watchlist-Project/src/components/ReviewModalWithButton.jsx
@@ -18,8 +18,19 @@ const handleShow = () => setShow(true);
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>Woohoo, you are reading this text in a modal!</div>
-          <RatingStarInput />
+          <div className='modal-img'>X We need to add image here X</div>
+          <div className='show-stats'>
+          <div>No. of Episodes:</div>
+          <div>No. of Seasons:</div>
+          <div>Country of Origin:</div>
+          <div>Genre:</div>
+          </div>
+          <div className='character-rating'> Characters: <RatingStarInput /></div> 
+          <div className='plot-rating'>Plot/Content: <RatingStarInput /></div>
+          <div className='writing-rating'>Writing: <RatingStarInput /></div>
+          <div className='pace-rating'>Pace: <RatingStarInput /> </div> 
+          <div className='review-txt'>Write your review here: </div>
+          <div className='overall-rating'>Overall rating: <RatingStarInput /></div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
