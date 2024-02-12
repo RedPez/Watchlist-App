805d0cc2ae9c269fac52e50ee2aceb8601cc5c65
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2024-02-12 16:43:19 +0000

    added more feature to the modal

--- Watchlist-Project/src/components/ReviewModalWithButton.jsx
+++ Watchlist-Project/src/components/ReviewModalWithButton.jsx
@@ -1,31 +1,42 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import RatingStarInput from "./RatingStarInput";
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import RatingStarInput from './RatingStarInput';
+import API from "../utils/API";
 
 const ReviewModalWithButton = (props) => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+const [show, setShow] = useState(false);
+const [showSeasons, setShowSeasons] = useState([]);
+console.log(showSeasons)
+const handleClose = () => setShow(false);
+const handleShow = async () => {
+
+    try {
+        const response =  await API.getShowSeasons(props.show.id)
+        setShowSeasons(response.data);
+      } catch (error) {
+       console.log(error)
+      }
+    
+  setShow(true)};
+
 
   return (
     <>
-      <Button class="review-btn" variant="primary" onClick={handleShow}>
-        Review
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
+    <Button variant="primary" onClick={handleShow}>Review</Button>
+    <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{props.title}</Modal.Title>
+          <Modal.Title>{props.show.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="modal-img">X We need to add image here X</div>
-          <div className="show-stats">
-            <div>No. of Episodes:</div>
-            <div>No. of Seasons:</div>
-            <div>Country of Origin:</div>
-            <div>Genre:</div>
+          <div className='modal-img'><img src={props.show.image.original} alt={props.show.name} /></div>
+          <div className='show-stats'>
+          <div>Date Released: {new Intl.DateTimeFormat('en-GB').format(new Date(props.show.premiered))}</div>
+          <div>No. of Seasons: {showSeasons.length}</div>
+          <div>Country of Origin: {props.show.network.country.name}</div>
+          <div>Genre: {props.show.genres.join(", ")}</div>
           </div>
+
           <div className="character-rating">
             {" "}
             Characters: <RatingStarInput />
@@ -39,7 +50,16 @@ const ReviewModalWithButton = (props) => {
           <div className="pace-rating">
             Pace: <RatingStarInput />{" "}
           </div>
-          <div className="review-txt">Write your review here: </div>
+          <div className="review-txt">Write your review here: 
+          <textarea 
+          rows="4" 
+          cols="50" 
+          name="comment" 
+          form="userform"
+          style={{border: "1px solid black"}}
+          >
+          </textarea>
+          </div>
           <div className="overall-rating">
             Overall rating: <RatingStarInput />
           </div>
