0651a908676c044c69543ab083968ff0285a805f
unknown <pjrbusiness@outlook.com>
2024-02-14 01:07:17 +0000

    changes to styling on nav hero homepage

--- Watchlist-Project/src/components/Hero.jsx
+++ Watchlist-Project/src/components/Hero.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <div>
-      <div className='hero-txt'> Welcome to <span className='brand'> Watchlist</span>, where your reviews are as important as the critics. Loved a tv show you watched recently? Or was it enough to put you to sleep - <span className='sleep'>Zzzz</span>. Either way, share it here. <br/> <span className='cta-txt'> Search for a tv show below to get started!</span> ↴ </div>
-      <video muted autoPlay loop className='hero-vid' onClick={handleClick} src="../src/assets/images/hero-video.mp4" title="hero-video"></video>
+      <div className="hero-txt">
+        {" "}
+        Welcome to <span className="brand"> Watchlist</span>, where your reviews
+        are as important as the critics. Loved a tv show you watched recently?
+        Or was it enough to put you to sleep -{" "}
+        <span className="sleep">Zzzz</span>. Either way, share it here. <br />{" "}
+        <span className="cta-txt">
+          {" "}
+          Search for a tv show below to get started!
+        </span>{" "}
+        ↴{" "}
+      </div>
+      <video
+        muted
+        autoPlay
+        loop
+        className="hero-vid"
+        onClick={handleClick}
+        src="../src/assets/images/hero-video.mp4"
+        title="hero-video"
+      ></video>
     </div>
-  )
-}
+  );
+};
 
 const handleClick = (event) => {
   if (event.target.paused) {
-    event.target.play()
+    event.target.play();
   } else {
-    event.target.pause()
+    event.target.pause();
   }
-}
+};
 
-export default Hero
+export default Hero;
--- Watchlist-Project/src/components/Navbar.jsx
+++ Watchlist-Project/src/components/Navbar.jsx
@@ -2,30 +2,31 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import './Navbar.css';
-import Homepage from '../pages/Homepage'
-import MyReviews from '../pages/MyReviews';
-import WatchList from '../pages/WatchList';
+import "./Navbar.css";
+import Homepage from "../pages/Homepage";
+import MyReviews from "../pages/MyReviews";
+import WatchList from "../pages/WatchList";
 
 function MyNavbar() {
   return (
     <>
-      <Navbar>
+      <Navbar className="custom-navbar">
         <Container>
           <Navbar.Brand href="/">
-            <img
-              alt=""
-              src="../src/assets/images/site-logo.png"
-              width="115"
-            />{" "}
+            <img alt="" src="../src/assets/images/site-logo.png" width="80" />{" "}
           </Navbar.Brand>
 
           <Nav className="nav-links-group">
-            <Nav.Link className="home-link" href="/">Home</Nav.Link>
-            <Nav.Link className="reviews-link" href="MyReviews">My Reviews</Nav.Link>
-            <Nav.Link className="watchlist-link" href="Watchlist">Watchlist</Nav.Link>
-            </Nav>
-            
+            <Nav.Link className="home-link" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="reviews-link" href="MyReviews">
+              My Reviews
+            </Nav.Link>
+            <Nav.Link className="watchlist-link" href="Watchlist">
+              Watchlist
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </>
