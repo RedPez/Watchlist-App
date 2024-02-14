d6f5be1e182ac4b7405de41fb83430665222ae3f
unknown <pjrbusiness@outlook.com>
2024-02-14 21:50:30 +0000

    Changed error with missing tags

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -8,26 +8,28 @@ import noImage from "../assets/images/no_image_avail.png";
 
 const DisplayCard = (props) => {
   return (
-    
+    <Container>
+      <Row>
+        <Col>
           <Card className="single-card">
-              <Card.Img 
+            <Card.Img
               // we need to change image for shows without images from the API
-                src={props.image?.original ? props.image.original : noImage}
-                alt={props.name}
-                className="card-img"
-              />
-            
+              src={props.image?.original ? props.image.original : noImage}
+              alt={props.name}
+              className="card-img"
+            />
+
             <Card.ImgOverlay className="hover-text">
               <Card.Title className="card-title">{props.name}</Card.Title>
               <div className="result-btns">
-                {props.action && <props.action 
-                show={props}
-                />}
-                
-                <ReviewModalWithButton show={props} review={props.review} buttonName= {props.buttonName} />
-                
-                </div>
+                {props.action && <props.action show={props} />}
 
+                <ReviewModalWithButton
+                  show={props}
+                  review={props.review}
+                  buttonName={props.buttonName}
+                />
+              </div>
             </Card.ImgOverlay>
           </Card>
         </Col>
